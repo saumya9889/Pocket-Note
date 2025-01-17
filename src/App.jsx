9d345ec3262e5209d3fdfcbb9d8855e33ef35e06
saumya9889 <msaumya114@gmail.com>
2025-01-18 00:38:55 +0530

    updated code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HomePage } from './Component/Home Page/HomePage';
+import "./assets/styles/global.css";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Home Page/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="main-page">
+          <div className="main-page-content">
+
+          
+
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
